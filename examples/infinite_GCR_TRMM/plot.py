f6d4499c4073b2f60eed56f556d76ff57a28909d
Ilham Variansyah <ilhamv@umich.edu>
2018-03-05 13:06:36 -0500

    Finally fix the BUG!

--- examples/infinite_GCR_TRMM/plot.py
+++ examples/infinite_GCR_TRMM/plot.py
@@ -5,10 +5,13 @@ import numpy as np
 f = h5py.File('output.h5', 'r');
 
 k = f['ksearch/k_cycle'];
+N = np.array(f['summary/Nsample']);
+NG = int(N**0.5);
 plt.plot(k,'x');
 plt.xlabel("cycle#");
 plt.ylabel("k");
 plt.ylim(0)
+plt.title('G=%s, sample=%s'%(NG,N))
 plt.show(); 
 
 flux = f['MG/flux/mean'];
@@ -23,6 +26,7 @@ plt.loglog(energy,flux[0]);
 plt.xlabel("Energy, MeV");
 plt.ylabel("Scalar flux");
 plt.grid();
+plt.title('G=%s, sample=%s'%(NG,N))
 plt.show(); 
 
 x = f['TRMM/alpha/real'];
@@ -32,22 +36,38 @@ plt.plot(x,y,'o');
 plt.xlabel("Re");
 plt.ylabel("Im");
 plt.grid();
+plt.title('G=%s, sample=%s'%(NG,N))
 plt.show(); 
 
 flux = f['TRMM/flux'];
 energy = f['MG/energy'];
 
+f_td = h5py.File('TDMC.h5', 'r');
+flux_td = f_td['spectrum/flux/mean'];
+energy_td = f_td['spectrum/energy'];
+
+energy_td = np.array(energy_td)*1E-6;
+energy_td = (energy_td[1:] + energy_td[:-1])/2;
+
+plt.scatter(energy_td,flux_td[0][0], s=80, facecolors='none', edgecolors='blue');
+plt.scatter(energy_td,flux_td[1][0], s=80, facecolors='none', edgecolors='orange');
+plt.scatter(energy_td,flux_td[2][0], s=80, facecolors='none', edgecolors='g');
+plt.scatter(energy_td,flux_td[3][0], s=80, facecolors='none', edgecolors='r');
+
+
+
 energy = np.array(energy)*1E-6;
 energy = (energy[1:] + energy[:-1])/2;
 
-plt.loglog(energy,flux[0]);
+#plt.loglog(energy,flux[0]);
 plt.loglog(energy,flux[1]);
 plt.loglog(energy,flux[2]);
 plt.loglog(energy,flux[3]);
 plt.loglog(energy,flux[4]);
-plt.ylim(1,);
+plt.ylim(1,1E12);
 plt.xlim(1E-9,2E1);
 plt.xlabel("Energy, MeV");
 plt.ylabel("Scalar flux");
 plt.grid();
+plt.title('G=%s, sample=%s'%(NG,N))
 plt.show();
